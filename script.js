846ac0221f03cea5bf196cf231153bdc064353b6
ChristopherZalapa <chriszalapa@gmail.com>
2021-01-13 12:50:40 -0800

    Loading Function

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick A Random Quote From apiQuotes Array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check If Author Field Is Black And Replace It With Unkown
@@ -23,11 +36,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quote From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
 
     try {
