22835c003728bc8b1f2230bd6793b919b112d9e8
ChristopherZalapa <chriszalapa@gmail.com>
2021-01-13 12:15:14 -0800

    New Quote API

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick A Random Quote From apiQuotes Array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get Quote From API
-async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+
     try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.log('Whoops, now quote', error);
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }   catch (error) {
+        // Catch Error Here
     }
 }
 
 // On Load
-getQuote();
+getQuotes();
